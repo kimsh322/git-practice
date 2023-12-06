e896494b7c0d1997035a45a5bda44fe7546b440a
kimsh322 <kimsh91322@gmail.com>
2023-12-06 18:57:00 +0900

    orange

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 
 function App() {
+  const a = "컨플릭 생겨라!";
   return (
     <>
       <div>깃 연습용 프로젝트</div>
       <div>apple</div>
       <div>banana</div>
       <div>토마토</div>
+      <div>{a}</div>
       <div>grape</div>
       <div>grape2</div>
       <div>grape3</div>
